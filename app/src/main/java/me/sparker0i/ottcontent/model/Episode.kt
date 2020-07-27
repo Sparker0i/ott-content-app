af181cbeb6673bb2c8d81baba4db2eab97484290
Aaditya Menon <aaditya@sparker0i.me>
2020-07-27 11:59:46 +0530

    Updated Show and Episode

--- app/src/main/java/me/sparker0i/ottcontent/model/Episode.kt
+++ app/src/main/java/me/sparker0i/ottcontent/model/Episode.kt
@@ -1,2 +1,19 @@
 package me.sparker0i.ottcontent.model
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import me.sparker0i.ottcontent.utils.Constants
+import java.util.*
+
+@JsonClass(generateAdapter = true)
+data class Episode(
+    @Json(name = Constants.ID) var id: Int,
+    @Json(name = Constants.SHOW_ID) var showId: Int,
+    @Json(name = Constants.SEASON_NUM) var seasonNum: Int,
+    @Json(name = Constants.EPISODE_NUM) var episodeNum: Int,
+    @Json(name = Constants.NAME) var name: String,
+    @Json(name = Constants.DESCRIPTION) var description: String,
+    @Json(name = Constants.AIR_DATE) var airDate: Date,
+    @Json(name = Constants.TRAILER) var trailer: String,
+    @Json(name = Constants.POSTER) var poster: String
+)
--- app/src/main/java/me/sparker0i/ottcontent/model/Show.kt
+++ app/src/main/java/me/sparker0i/ottcontent/model/Show.kt
@@ -1,4 +1,13 @@
 package me.sparker0i.ottcontent.model
 
-class Show {
-}
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import me.sparker0i.ottcontent.utils.Constants
+
+@JsonClass(generateAdapter = true)
+data class Show(
+    @Json(name = Constants.ID) var id: Int,
+    @Json(name = Constants.NAME) var name: String,
+    @Json(name = Constants.DESCRIPTION) var description: String,
+    @Json(name = Constants.TRAILER) var trailer: String
+)
