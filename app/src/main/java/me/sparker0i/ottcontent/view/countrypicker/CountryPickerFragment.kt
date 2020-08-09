69bc77d3f611dfa7dc1d76158c283ff2f02ed94c
Aaditya Menon <aaditya@sparker0i.me>
2020-08-09 20:08:07 +0530

    New Country Picker Adaptation

--- app/src/main/java/me/sparker0i/ottcontent/view/countrypicker/CountryPickerFragment.kt
+++ app/src/main/java/me/sparker0i/ottcontent/view/countrypicker/CountryPickerFragment.kt
@@ -1,21 +1,24 @@
 package me.sparker0i.ottcontent.view.countrypicker
 
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.coroutines.launch
 import me.sparker0i.ottcontent.R
 import me.sparker0i.ottcontent.model.Country
-import me.sparker0i.ottcontent.view.CountryAdapter
 import me.sparker0i.ottcontent.view.base.ScopedFragment
+import me.sparker0i.ottcontent.view.countrypicker.adapter.CountryAdapter
 import me.sparker0i.ottcontent.viewmodel.ContentViewModel
 import me.sparker0i.ottcontent.viewmodel.ContentViewModelFactory
 import org.kodein.di.KodeinAware
@@ -25,8 +28,11 @@ import org.kodein.di.generic.instance
 class CountryPickerFragment : ScopedFragment(), KodeinAware {
     override val kodein by closestKodein()
 
+    private var mutableCountries = MutableLiveData<List<Country>>()
+
     private val viewModelFactory: ContentViewModelFactory by instance()
     private lateinit var viewModel: ContentViewModel
+    private lateinit var selectionTracker: SelectionTracker<Long?>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,7 +49,31 @@ class CountryPickerFragment : ScopedFragment(), KodeinAware {
         bindUi()
     }
 
+    private fun setupRecyclerView(recyclerView: RecyclerView, countries: List<Country>) {
+        val adapter = CountryAdapter()
+        adapter.items = countries
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.itemAnimator = DefaultItemAnimator()
+
+        selectionTracker = SelectionTracker.Builder(
+            "country_selection",
+            recyclerView,
+            CountryAdapter.KeyProvider(adapter),
+            CountryAdapter.DetailsLookup(recyclerView),
+            StorageStrategy.createLongStorage()
+        )
+            .withSelectionPredicate(SelectionPredicates.createSelectSingleAnything())
+            .build()
+
+        adapter.selectionTracker = selectionTracker
+    }
+
     private fun bindUi() {
+        mutableCountries.observe(viewLifecycleOwner, Observer {list ->
+            setupRecyclerView(country_recycler_view, list)
+        })
+
         getCountries()
     }
 
@@ -51,18 +81,7 @@ class CountryPickerFragment : ScopedFragment(), KodeinAware {
         val countries = viewModel.countries.await()
 
         countries.observe(viewLifecycleOwner, Observer {list ->
-            prepareRecyclerView(list)
+            mutableCountries.postValue(list)
         })
-    }
-
-    private fun prepareRecyclerView(countryList: List<Country>?) {
-        val countryAdapter = CountryAdapter(countryList)
-
-        if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
-            country_recycler_view.layoutManager = LinearLayoutManager(context)
-        else
-            country_recycler_view.layoutManager = GridLayoutManager(context, 4)
-        country_recycler_view.itemAnimator = DefaultItemAnimator()
-        country_recycler_view.adapter = countryAdapter
     }
 }
